0bf757ae67a64cc257ec724b99ae8eb21df194be
Amruthesh V K <9074841649b@gmail.com>
2025-10-26 17:01:26 +0530

    Changed registration page and fixed passwordencryption

--- src/main/java/com/example/buspassmanagement/security/CustomUserDetailsService.java
+++ src/main/java/com/example/buspassmanagement/security/CustomUserDetailsService.java
@@ -1,6 +1,5 @@
 package com.example.buspassmanagement.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,8 +11,11 @@ import com.example.buspassmanagement.service.UserService;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
